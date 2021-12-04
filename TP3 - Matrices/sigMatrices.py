c01710348d45fa7957e9b353f85c48f1a02c9b0c
Matias <matiaslorenzo466@gmail.com>
2021-12-04 19:20:25 -0300

    se agregaron funciones matrices
    se agregaron funciones matrices

--- TP3 - Matrices/sigMatrices.py
+++ TP3 - Matrices/sigMatrices.py
@@ -39,10 +39,74 @@ def ejercicioA(matriz):
         contador+=2
     return matriz
 
+# SEGUNDA TABLA
 
+def ejercicioB(matriz):
+    cont = 3
+    fila = len(matriz)
+    columna = len(matriz[0])
+    for f in range(fila):
+        for c in range(columna):
+            if f+c == fila-1:
+                matriz[f][c] = 3**cont
+                cont-=1
+    return matriz
+
+# Tercera TABLA
+
+def ejercicioC(matriz):
+    contador = 4
+    fila = len(matriz)
+    columna = len(matriz[0])
+    for f in range(fila):
+        for c in range(columna):
+            if f>=c:
+                matriz[f][c] = contador
+        contador-=1
+    return matriz
+
+# CUARTA TABLA
+
+def ejercicioD(matriz):
+    contador = 8
+    fila = len(matriz)
+    columna = len(matriz[0])
+    for f in range(fila):
+        for c in range(columna):
+            matriz[f][c] = contador
+        contador/=2
+    return matriz        
+
+def ejercicioE(matriz):
+    contador = 1
+    fila = len(matriz)
+    columna = len(matriz[0])
+    for f in range(fila):
+        for c in range(columna):
+            if f % 2 == 0 and c % 2 != 0: 
+                matriz[f][c] = contador
+                contador += 1
+            if f % 2 != 0 and c % 2 == 0:
+                matriz[f][c] = contador
+                contador += 1
+    return matriz
+
+def ejercicioF(matriz):
+    cont = 0
+    for f in range(len(matriz)):
+        for c in range(4-1,-1,-1):
+            if (c >=4-1-f):
+                matriz[f][c] = cont
+                cont+=1
+    return matriz
 
 def main():
-    matriz = crearMatriz(5)
-    ejercicioA(matriz)
+    matriz = crearMatriz(4)
+    # ejercicioA(matriz)
+    # ejercicioB(matriz)
+    # ejercicioC(matriz)
+    # ejercicioD(matriz)
+    # ejercicioE(matriz)
+    ejercicioF(matriz)
     imprimirMatriz(matriz)
 main()
